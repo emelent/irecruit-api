91644105c1c2288faec323edaf8df707faf423d1
Roy <technicmk@gmail.com>
2018-08-02 15:02:05 +0200

    Setup unique indexes in collections

--- database/database.go
+++ database/database.go
@@ -1,11 +1,46 @@
 package database
 
-import mgo "gopkg.in/mgo.v2"
+import (
+	config "../config"
+	mgo "gopkg.in/mgo.v2"
+)
 
 //NewCRUD creates a new CRUD type
 func NewCRUD(session *mgo.Session) *CRUD {
+	clone := session.Copy()
+	defer clone.Close()
+
+	ensureIndexes(clone)
 	crud := &CRUD{}
 	crud.Session = session
 	crud.TempStorage = make(map[string][]interface{})
 	return crud
+}
+
+var collectionIndexes = map[string][]mgo.Index{
+	"accounts": []mgo.Index{
+		{
+			Key:    []string{"email"},
+			Unique: true,
+		},
+	},
+	"token_managers": []mgo.Index{
+		{
+			Key:    []string{"account_id"},
+			Unique: true,
+		},
+		{
+			Key:    []string{"tokens.signature"},
+			Unique: true,
+		},
+	},
+}
+
+func ensureIndexes(session *mgo.Session) {
+	for name, indexes := range collectionIndexes {
+		c := session.DB(config.DbName).C(name)
+		for _, index := range indexes {
+			c.EnsureIndex(index)
+		}
+	}
 }
