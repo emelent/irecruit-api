ecee549daec740958758c66c5d4fc4102d94a014
Roy <technicmk@gmail.com>
2018-08-03 15:55:21 +0200

    Remove Token model, just deal with token manager

--- models/tokenManager.go
+++ models/tokenManager.go
@@ -1,33 +1,15 @@
 package models
 
 import (
-	e "../errors"
 	"gopkg.in/mgo.v2/bson"
 )
-
-// Token model
-type Token struct {
-	Signature string `json:"signature" bson:"signature"`
-	Device    string `json:"device" bson:"device"`
-	IP        string `json:"ip" bson:"ip"`
-	UserAgent string `json:"user_agent" bson:"user_agent"`
-}
-
-//OK validates Token fields
-func (t *Token) OK() error {
-	if t.Signature == "" {
-		return e.NewMissingFieldError("Signature")
-	}
-	return nil
-}
 
 // TokenManager model
 type TokenManager struct {
 	AccountID    bson.ObjectId `json:"account_id" bson:"account_id"`
-	Tokens       []Token       `json:"tokens" bson:"tokens"`
-	RefreshToken Token         `json:"refresh_token" bson:"refresh_token"`
-
-	maxTokens int
+	Tokens       []string      `json:"tokens" bson:"tokens"`
+	RefreshToken string        `json:"refresh_token" bson:"refresh_token"`
+	MaxTokens    int           `json:"max_tokens" bson:"max_tokens"`
 }
 
 // OK validates token manager
