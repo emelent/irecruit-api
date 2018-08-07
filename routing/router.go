47bd9a1a7a1b432f440f29d2a164aad5bbb3fc12
Roy <technicmk@gmail.com>
2018-08-07 19:45:45 +0200

    Decouple graphqlhandler from  router creation

--- routing/router.go
+++ routing/router.go
@@ -13,18 +13,24 @@ import (
 	schemas "../schemas"
 )
 
-// NewGqlRouter prepares a new router with necessary endpoints
-func NewGqlRouter(crud *db.CRUD, middleware ...mware.Middleware) http.Handler {
-
-	// prepare graphql handler
+// NewGqlHandler creates a graphql handler
+func NewGqlHandler(crud *db.CRUD) http.Handler {
+	// prepare graphql root resolver
 	gqlResolver := &resolver.RootResolver{}
 	gqlResolver.Init(crud)
 
+	// create schema
 	schema := graphql.MustParseSchema(
 		schemas.CreateSchema(schemas.DefaultSchemas...),
 		gqlResolver,
 	)
 
+	// make handler
+	return mware.ReqInfoMiddleware(&relay.Handler{Schema: schema})
+}
+
+// NewGqlRouter prepares a new router with necessary endpoints
+func NewGqlRouter(crud *db.CRUD, middleware ...mware.Middleware) http.Handler {
 	// prepare router
 	router := mux.NewRouter()
 	mware.ApplyMiddleware(router, middleware...)
@@ -33,7 +39,7 @@ func NewGqlRouter(crud *db.CRUD, middleware ...mware.Middleware) http.Handler {
 	router.
 		Path("/graphql").
 		// Methods(http.MethodPost). // use post-only in production
-		Handler(mware.ReqInfoMiddleware(&relay.Handler{Schema: schema}))
+		Handler(NewGqlHandler(crud))
 
 	return router
 }
