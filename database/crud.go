3dfd72651e849d9f40df1eca5a30bd13234dfc71
Roy <technicmk@gmail.com>
2018-08-08 20:01:17 +0200

    return errors on failed crud operations

--- database/crud.go
+++ database/crud.go
@@ -2,9 +2,15 @@ package database
 
 import (
 	config "../config"
+	er "../errors"
 	"github.com/fatih/structs"
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+)
+
+const (
+	errBadCollection = "Invalid collection name."
+	errNotFound      = "Not found."
 )
 
 //CRUD is a db abstraction layer used to perforom testing
@@ -43,6 +49,12 @@ func (db *CRUD) Insert(collection string, values ...interface{}) error {
 //FindAll  finds all matching db entries
 func (db *CRUD) FindAll(collection string, query *bson.M) ([]interface{}, error) {
 	if db.Session == nil { // a.k.a, we're in the mock
+
+		// check if collection exists
+		if _, ok := db.TempStorage[collection]; !ok {
+			return nil, er.NewCRUDError(errBadCollection)
+		}
+
 		results := filter(db.TempStorage[collection], matchQuery(query))
 		return results, nil
 	}
@@ -56,8 +68,19 @@ func (db *CRUD) FindAll(collection string, query *bson.M) ([]interface{}, error)
 //FindOne finds a db entry
 func (db *CRUD) FindOne(collection string, query *bson.M) (interface{}, error) {
 	if db.Session == nil { // in the mock
+
+		// check if collection exists
+		if _, ok := db.TempStorage[collection]; !ok {
+			return nil, er.NewCRUDError(errBadCollection)
+		}
+
+		// find the result
 		result := filterFirst(db.TempStorage[collection], matchQuery(query))
-		return result, nil
+		var err error
+		if result == nil {
+			err = er.NewCRUDError(errNotFound)
+		}
+		return result, err
 	}
 
 	db.InitCopy()
@@ -69,8 +92,19 @@ func (db *CRUD) FindOne(collection string, query *bson.M) (interface{}, error) {
 //FindID finds a db entry by ID
 func (db *CRUD) FindID(collection string, id interface{}) (interface{}, error) {
 	if db.Session == nil { // in the mock
+
+		// check if collection exists
+		if _, ok := db.TempStorage[collection]; !ok {
+			return nil, er.NewCRUDError(errBadCollection)
+		}
+
+		// find the result
 		result := filterFirst(db.TempStorage[collection], matchID(id))
-		return result, nil
+		var err error
+		if result == nil {
+			err = er.NewCRUDError(errNotFound)
+		}
+		return result, err
 	}
 
 	db.InitCopy()
@@ -82,14 +116,26 @@ func (db *CRUD) FindID(collection string, id interface{}) (interface{}, error) {
 //UpdateID updates entry by id
 func (db *CRUD) UpdateID(collection string, id bson.ObjectId, updates bson.M) error {
 	if db.Session == nil { // mocking
+
+		// check if collection exists
+		if _, ok := db.TempStorage[collection]; !ok {
+			return er.NewCRUDError(errBadCollection)
+		}
+
+		// perform update
+		found := false
 		for i, r := range db.TempStorage[collection] {
 			if r["_id"] == id {
 				for k, v := range updates {
 					r[k] = v
 				}
 				db.TempStorage[collection][i] = r
+				found = true
 				break
 			}
+		}
+		if !found {
+			return er.NewCRUDError(errNotFound)
 		}
 		return nil
 	}
@@ -100,16 +146,27 @@ func (db *CRUD) UpdateID(collection string, id bson.ObjectId, updates bson.M) er
 
 //DeleteID deletes a db entry by id
 func (db *CRUD) DeleteID(collection string, id bson.ObjectId) error {
-	//mock
-	if db.Session == nil {
+
+	if db.Session == nil { //I'm mocking  here, I'm mocking
+
+		// check if collection exists
+		if _, ok := db.TempStorage[collection]; !ok {
+			return er.NewCRUDError(errBadCollection)
+		}
+
 		c := db.TempStorage[collection]
+		found := false
 		for i, v := range db.TempStorage[collection] {
 			if v["_id"] == id {
 				db.TempStorage[collection] = append(c[:i], c[i+1:]...)
+				found = true
 				break
 			}
 		}
 
+		if !found {
+			return er.NewCRUDError(errNotFound)
+		}
 		return nil
 	}
 
@@ -133,6 +190,8 @@ func (db *CRUD) CloseCopy() {
 		db.CopySession = nil
 	}
 }
+
+// helpers
 
 func makeBson(in interface{}) bson.M {
 	structs.DefaultTagName = "bson"
--- tests/unit/crud_test.go
+++ tests/unit/crud_test.go
@@ -60,7 +60,7 @@ func TestCrudFindOne(t *testing.T) {
 	// prepare results
 	i1, _ := crud.FindOne(collection, nil)                          // expect one
 	i2, _ := crud.FindOne(collection, &bson.M{"Food": "Ice-Cream"}) // expect one
-	r3, _ := crud.FindOne(collection, &bson.M{"Name": "Jake"})      // expect none
+	r3, err := crud.FindOne(collection, &bson.M{"Name": "Jake"})    // expect none
 
 	r1 := i1.(bson.M)
 	r2 := i2.(bson.M)
@@ -70,6 +70,7 @@ func TestCrudFindOne(t *testing.T) {
 	assert.Equal(p0["_id"], r1["_id"], "crud.FindOne with nil query does not return expected result")
 	assert.Equal(p0["_id"], r2["_id"], "crud.FindOne with matching query does not return expected result")
 	assert.Nil(r3, "crud.FindOne with non-matching query does not return expected result")
+	assert.NotNil(err, "crud.FindOne doesn't return an error on nil result")
 }
 
 func TestCrudFindID(t *testing.T) {
@@ -79,12 +80,13 @@ func TestCrudFindID(t *testing.T) {
 	// prepare results
 	i1, _ := crud.FindID(collection, p0["_id"]) // expect one                         // expect one
 	r1 := i1.(bson.M)
-	r2, _ := crud.FindID(collection, nil) // expect none
+	r2, err := crud.FindID(collection, nil) // expect none
 
 	//make assertions
 	assert := assert.New(t)
 	assert.Equal(p0["_id"], r1["_id"], "crud.FindID with nil query does not return expected result")
 	assert.Nil(r2, "crud.FindID with non-matching query does not return nil")
+	assert.NotNil(err, "crud.FindID doesn't return an error on nil result")
 }
 
 func TestCrudUpdateID(t *testing.T) {
@@ -97,12 +99,14 @@ func TestCrudUpdateID(t *testing.T) {
 	crud.UpdateID(collection, p0.ID, bson.M{"Name": newName})
 	i1, _ := crud.FindOne(collection, &bson.M{"_id": p0.ID})
 	r1 := i1.(bson.M)
+	err := crud.UpdateID(collection, bson.NewObjectId(), bson.M{})
 
 	// make asserts
 	assert := assert.New(t)
 	assert.NotEqual(oldName, newName, "New name and old name are the same.")
 	assert.NotEqual(oldName, r1["Name"], "crud.UpdateID did not update the old value.")
 	assert.Equal(newName, r1["Name"], "crud.UpdateID did not update to the correct value.")
+	assert.NotNil(err, "crud.UpdateID doesn't return an error on nil result")
 
 }
 
@@ -114,9 +118,11 @@ func TestCrudDeleteID(t *testing.T) {
 	b1, _ := crud.FindOne(collection, &bson.M{"_id": p0.ID})
 	crud.DeleteID(collection, p0.ID)
 	r1, _ := crud.FindOne(collection, &bson.M{"_id": p0.ID})
+	err := crud.DeleteID(collection, bson.NewObjectId())
 
 	// make asserts
 	assert := assert.New(t)
 	assert.NotNil(b1, "An expected value does not exist in the db.")
 	assert.Nil(r1, "crud.DeleteID did not remove value from the db.")
+	assert.NotNil(err, "crud.DeleteID doesn't return an error on nil result")
 }
