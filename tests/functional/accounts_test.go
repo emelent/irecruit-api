5a89629378f7af3be43f0327e50c0c5f0d0e306e
Roy <technicmk@gmail.com>
2018-08-09 08:30:36 +0200

    Add invalide createAccountTest

--- tests/functional/accounts_test.go
+++ tests/functional/accounts_test.go
@@ -28,15 +28,15 @@ func TestAccountList(t *testing.T) {
 	}
 
 	dataPortion, dOk := response["data"].(map[string]interface{})
-	results, rOk := dataPortion[method].([]interface{})
+	resultAccounts, rOk := dataPortion[method].([]interface{})
 
 	//make assertions
-	assert.NotContains(response, "error", msgUnexpectedError)
+	assert.NotContains(response, "errors", msgUnexpectedError)
 	assert.Contains(response, "data", msgInvalidResponse)
 	assert.Contains(response["data"], method, msgMissingResponseData)
 	assert.True(dOk, msgInvalidResponseType)
 	assert.True(rOk, fmt.Sprintf("Invalid data[\"%s\"] response type.", method))
-	assert.Len(results, len(accounts), msgInvalidResultCount)
+	assert.Len(resultAccounts, len(accounts), msgInvalidResultCount)
 }
 
 func TestRemoveAccountValid(t *testing.T) {
@@ -67,7 +67,7 @@ func TestRemoveAccountValid(t *testing.T) {
 	result, rOk := dataPortion[method].(string)
 
 	//make assertions
-	assert.NotContains(response, "error", msgUnexpectedError)
+	assert.NotContains(response, "errors", msgUnexpectedError)
 	assert.Contains(response, "data", msgInvalidResponse)
 	assert.Contains(response["data"], method, msgMissingResponseData)
 	assert.True(dOk, msgInvalidResponseType)
@@ -112,5 +112,122 @@ func TestRemoveAccountInvalid(t *testing.T) {
 }
 
 func TestCreateAccountValid(t *testing.T) {
+	//prepare handler
+	crud := loadedCrud()
+	handler := createGqlHandler(crud)
+
+	//prepare request
+	method := "createAccount"
+	queryFormat := `
+		mutation{
+			%s(%s){
+				refreshToken
+				accessToken
+			}
+		}
+	`
+
+	input := `
+		info: {
+			email: "test@gmail.com",
+			password:"test",
+			name: "Test",
+			surname:"User"
+		}
+	`
+	query := fmt.Sprintf(queryFormat, method, input)
+	//make request
+	response, err := gqlRequestAndRespond(handler, query, nil)
+
+	//process response
+	assert := assert.New(t)
+	if err != nil {
+		assert.Fail("Failed to process response:", err)
+	}
+
+	dataPortion, dOk := response["data"].(map[string]interface{})
+	resultTokens, rOk := dataPortion[method].(map[string]interface{})
+
+	//make assertions
+	assert.NotContains(response, "errors", msgUnexpectedError)
+	assert.Contains(response, "data", msgInvalidResponse)
+	assert.Contains(response["data"], method, msgMissingResponseData)
+	assert.True(dOk, msgInvalidResponseType)
+	assert.True(rOk, fmt.Sprintf("Invalid data[\"%s\"] response type.", method))
+	assert.Contains(resultTokens, "accessToken", msgMissingResponseData)
+	assert.Contains(resultTokens, "refreshToken", msgMissingResponseData)
+
+}
+
+func TestCreateAccountInvalid(t *testing.T) {
+	//prepare handler
+	crud := loadedCrud()
+	handler := createGqlHandler(crud)
+
+	//prepare request
+	method := "createAccount"
+	queryFormat := `
+		mutation{
+			%s(%s){
+				refreshToken
+				accessToken
+			}
+		}
+	`
+
+	input := []string{
+		`
+			# missing field, (email)
+			info: {
+				password:"test",
+				name: "Test",
+				surname:"User"
+			}
+		`,
+		`
+			# invalid data type
+			info: {
+				email: 14,
+				password:"test",
+				name: "Test",
+				surname:"User"
+			}	
+		`,
+		`
+			# invalid data content
+			info: {
+				email: "marshia",
+				password:"test",
+				name: "Test",
+				surname:"User"
+			}	
+		`,
+
+		// can't test duplicate entries because mock doesn't have that infrastructure
+		// error is on the db layer
+		// fmt.Sprintf(`
+		// 	# duplicate email
+		// 	info: {
+		// 		email: "%s",
+		// 		password:"test",
+		// 		name: "Test",
+		// 		surname:"User"
+		// 	}
+		// `, accounts[0].Email),
+	}
+	for i, in := range input {
+		query := fmt.Sprintf(queryFormat, method, in)
+		//make request
+		response, err := gqlRequestAndRespond(handler, query, nil)
+
+		//process response
+		assert := assert.New(t)
+		if err != nil {
+			assert.Fail("Failed to process response:", err)
+		}
+
+		//make assertions
+		assert.Contains(response, "errors", fmt.Sprintf("Case [%v]: %s", i, msgUnexpectedError))
+	}
 
 }
