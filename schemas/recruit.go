bf4a24a88e868085f23f95b8c5f3e3709a8baa83
Roy <technicmk@gmail.com>
2018-08-11 14:43:21 +0200

    Create recruit gql schema

---
+++ schemas/recruit.go
@@ -0,0 +1,62 @@
+package schemas
+
+// RecruitSchema graphql schema
+var RecruitSchema = Schema{
+	Types: `
+		type QA{
+			question: String!
+			answer: String!
+		}
+
+		type Recruit{
+			id: ID!
+			surname: String!
+			phone: String!
+			name: String!
+			email: String!
+			# dob: Date
+			province: String!
+			city: String!
+			gender: String!
+			disability: String!
+			vid1_url: String!
+			vid2_url: String!		
+			#qa1: QA!
+			#qa2: QA!
+		}
+
+		input RecruitDetails{
+			province: Province
+			city: String
+			gender: String
+			disability: String
+			vid1_url: String
+			vid2_url: String
+			qa1_question: String
+			qa1_answer: String
+			qa2_question: String
+			qa2_answer: String
+		}
+
+		enum Province{
+			KWAZULU_NATAL
+			NORTHERN_CAPE
+			WESTERN_CAPE
+			EASTERN_CAPE
+			NORTH_WEST
+			FREE_STATE
+			MPUMALANGA
+			GAUTENG
+			LIMPOPO
+		}
+	`,
+
+	Queries: `
+		recruits: [Recruit]!
+	`,
+	Mutations: `
+		createRecruit(account_id: ID!, info: RecruitDetails!): Recruit
+		#removeRecruit(id: ID): String
+
+	`,
+}
