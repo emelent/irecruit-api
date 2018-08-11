fab7e273e1e8893dd485944cf9e216dc54c437c3
Roy <technicmk@gmail.com>
2018-08-11 21:15:16 +0200

    Add NullObject

--- models/model.go
+++ models/model.go
@@ -1,5 +1,12 @@
 package models
 
+import (
+	"gopkg.in/mgo.v2/bson"
+)
+
+// NullObjectID Serves as place holder for null objectID
+var NullObjectID = bson.NewObjectId()
+
 // Model interface
 type Model interface {
 	OK() error
