7a1289341edec7adc695d0d9be2c5f2d2f56491a
Roy <technicmk@gmail.com>
2018-08-11 14:41:59 +0200

    Add some constants in the config

--- config/config.go
+++ config/config.go
@@ -1,13 +1,17 @@
 package config
 
 // TODO base all this on env files
-//
 
-// Port is the port where the server will listen
-const Port = ":9999"
+// database constants
+const (
+	Port   = ":9999"
+	DbHost = "localhost"
+	DbName = "mule"
+)
 
-//DbHost is the database host name
-const DbHost = "localhost"
-
-//DbName is the database name
-const DbName = "mule"
+// collections names
+const (
+	AccountsCollection      = "accounts"
+	TokenManagersCollection = "token_managers"
+	RecruitsCollection      = "recruits"
+)
