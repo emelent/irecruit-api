e9aee47575ad156f3df3bae513ef97e6a2d09c0c
Roy <technicmk@gmail.com>
2018-08-16 19:26:03 +0200

    Switch to using an env file

--- config/config.go
+++ config/config.go
@@ -1,12 +1,10 @@
 package config
 
-// TODO base all this on env files
+import (
+	"log"
+	"os"
 
-// database constants
-const (
-	Port   = ":9999"
-	DbHost = "localhost"
-	DbName = "mule"
+	"github.com/joho/godotenv"
 )
 
 // collections names
@@ -16,3 +14,18 @@ const (
 	RecruitsCollection      = "recruits"
 	IndustriesCollection    = "industries"
 )
+
+// SetupEnv ...
+func SetupEnv() {
+	env := string(os.Getenv("ENV"))
+	if env == "" {
+		log.Println("Environment variable `ENV` not set, falling back to default config.")
+		return
+	}
+	if err := godotenv.Load(".env." + env); err != nil {
+		log.Println("Failed to load env file, falling back to default config.")
+		os.Setenv("PORT", "9999")
+		os.Setenv("DB_HOST", "localhost")
+		os.Setenv("DB_NAME", "irecruit")
+	}
+}
