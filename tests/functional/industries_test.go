4776a61d16c0d4f4ea5bbeaba9f208f9a18c7838
Roy <technicmk@gmail.com>
2018-08-16 19:55:37 +0200

    Add industries test

--- tests/functional/industries_test.go
+++ tests/functional/industries_test.go
@@ -33,5 +33,86 @@ func TestIndustryList(t *testing.T) {
 	assert.Contains(response["data"], method, msgMissingResponseData)
 	assert.True(dOk, msgInvalidResponseType)
 	assert.True(rOk, fmt.Sprintf("Invalid data[\"%s\"] response type.", method))
-	assert.Len(resultIndustries, len(moc.Industries), msgInvalidResultCount)
+	if rOk {
+		assert.Len(resultIndustries, len(moc.Industries), msgInvalidResultCount)
+	}
+}
+
+func TestCreateIndustryValid(t *testing.T) {
+	handler := createLoadedGqlHandler()
+
+	// prepare request
+	method := "createIndustry"
+	name := "Test Industry"
+	query := fmt.Sprintf(`
+		mutation{
+			%s(name: "%s"){
+				id
+				name
+			}
+		}
+	`, method, name)
+
+	// request and respond
+	response, err := gqlRequestAndRespond(handler, query, nil)
+
+	//process response
+	assert := assert.New(t)
+	if err != nil {
+		assert.Fail("Failed to process response:", err)
+	}
+
+	dataPortion, dOk := response["data"].(map[string]interface{})
+	resultIndusty, rOk := dataPortion[method].(map[string]string)
+
+	//make assertions
+	assert.NotContains(response, "errors", msgUnexpectedError)
+	assert.Contains(response, "data", msgInvalidResponse)
+	assert.Contains(response["data"], method, msgMissingResponseData)
+	assert.True(dOk, msgInvalidResponseType)
+	assert.True(rOk, fmt.Sprintf("Invalid data[\"%s\"] response type.", method))
+
+	if rOk {
+		assert.Contains(resultIndusty, "id", msgMissingResponseData)
+		assert.Contains(resultIndusty, "name", msgMissingResponseData)
+		assert.Equal(resultIndusty["name"], name, msgInvalidResult)
+	}
+}
+
+func TestCreateIndustryInvalid(t *testing.T) {
+	handler := createLoadedGqlHandler()
+
+	// prepare request
+	method := "createIndustry"
+	queryFormat := `
+		mutation{
+			%s(%s){
+				id
+				name
+			}
+		}
+	`
+
+	// invalid inputs
+	input := []string{
+		`
+			# case 1 no name
+		`,
+		` 
+			# case 2 invalid name
+			name: ""
+		`,
+	}
+	for i, in := range input {
+		query := fmt.Sprintf(queryFormat, method, in)
+		// request and respond
+		response, err := gqlRequestAndRespond(handler, query, nil)
+		//process response
+		assert := assert.New(t)
+		if err != nil {
+			assert.Fail("Failed to process response:", err)
+		}
+
+		assert.Contains(response, "errors", fmt.Sprintf("Case [%v]: %s", i+1, msgNoError))
+	}
 }
