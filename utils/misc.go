240dd677969f87cd0389d2c854e374b8e17c5bdb
Roy <technicmk@gmail.com>
2018-08-18 19:29:05 +0200

    Add some util functions

--- utils/misc.go
+++ utils/misc.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"log"
+	"math/rand"
 
 	models "../models"
 	"gopkg.in/mgo.v2/bson"
@@ -20,4 +21,22 @@ func IsSysAccount(account *models.Account) bool {
 // IsNullID checks if given id references a "null" id
 func IsNullID(id bson.ObjectId) bool {
 	return id == models.NullObjectID
+}
+
+// PickRandomN create a random subset of length n from input slice
+func PickRandomN(n int, input []interface{}) []interface{} {
+	out := make([]interface{}, n)
+	for i := range out {
+		index := rand.Intn(len(input))
+		out[i] = input[index]
+		// remove used value from index
+		input = append(input[:index], input[index+1:]...)
+
+	}
+	return out
+}
+
+// PickRandom pick a random value from input slice
+func PickRandom(n int, input []interface{}) interface{} {
+	return PickRandomN(1, input)[0]
 }
